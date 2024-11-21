000490f449ac000f7563f5dc4c1173671ec11c7c
utsab dahal <utsabdahal190@gmail.com>
2024-11-21 16:21:32 +0545

    model update

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ with open('random_forest.pkl', 'rb') as model_file:
     model = pkl.load(model_file)
 
 st.title('Loan Approval Prediction')
-input_placeholder = st.empty()
 
 person_age = st.number_input('Age: ', max_value=100, value=None)
 person_gender = st.selectbox('Gender: ', encoders['person_gender'].classes_)
@@ -20,15 +19,15 @@ person_emp_exp = st.number_input('Employment Experience: ', value=None)
 person_home_ownership = st.selectbox('Home Ownership: ', encoders['person_home_ownership'].classes_)
 loan_amnt = st.number_input('Loan Amount: ', value=100)
 loan_intent = st.selectbox('Loan Intent: ', encoders['loan_intent'].classes_)
-loan_int_rate = st.number_input('Loan Interest Rate: ', step=0.1, value=None)
+loan_int_rate = st.number_input('Loan Interest Rate: ', step=0.1)
 cb_person_cred_hist_length = st.number_input('Credit History Length: ', value=None)
 credit_score = st.number_input('Credit Score: ', value=None)
 previous_loan_defaults_on_file = st.selectbox('Previous Loan Defaults: ', encoders['previous_loan_defaults_on_file'].classes_)
 
 loan_percent_income = loan_amnt / person_income 
-# loan_to_income = loan_amnt / person_income 
-# income_to_loan = person_income / loan_amnt 
-# net_disposable_income = person_income - (loan_amnt * loan_int_rate / 100)
+loan_to_income = loan_amnt / person_income 
+income_to_loan = person_income / loan_amnt 
+net_disposable_income = person_income - (loan_amnt * loan_int_rate / 100)
 
 
 encoded_data = {
@@ -44,15 +43,14 @@ encoded_data = {
     'loan_percent_income': loan_percent_income, 
     'cb_person_cred_hist_length': cb_person_cred_hist_length,
     'credit_score': credit_score, 
-    'previous_loan_defaults_on_file': encoders['previous_loan_defaults_on_file'].transform([previous_loan_defaults_on_file])[0]
-    
-    # 'loan_to_income': loan_to_income, 
-    # 'income_to_loan': income_to_loan, 
-    # 'net_disposable_income': net_disposable_income 
+    'previous_loan_defaults_on_file': encoders['previous_loan_defaults_on_file'].transform([previous_loan_defaults_on_file])[0],    
+    'loan_to_income': loan_to_income, 
+    'income_to_loan': income_to_loan, 
+    'net_disposable_income': net_disposable_income 
 }
 
 predict_df = pd.DataFrame([encoded_data])
-scaler = ['person_age', 'person_income', 'loan_amnt', 'loan_int_rate', 'cb_person_cred_hist_length', 'credit_score']
+scaler = ['person_age', 'person_income', 'loan_amnt', 'loan_int_rate', 'cb_person_cred_hist_length', 'credit_score', 'loan_to_income', 'income_to_loan', 'net_disposable_income']
 # scaler = StandardScaler()
 predict_df[scaler] = StandardScaler().fit_transform(predict_df[scaler])
 
