c0c84230e9f777083a06022273ee1a1ebb5cbbe0
utsab dahal <utsabdahal190@gmail.com>
2024-12-16 12:31:26 +0545

    multicolinearity addressed

--- README.md
+++ README.md
@@ -10,35 +10,41 @@ This project predicts loan approval status using machine learning classification
     2. Outlier Removed (Age above 100 and income above 1 million)
 
 ## Feature Engineering
-    1. Two new column added: income_to_loan ratio and net_disposable_income
+    1. New column added: net_disposable_income
 
 ## EDA
+
 1. Loan Status According to Gender
 ![alt text](charts/loan_status_gender.png)
+
 
 2. Loan Status According to Education Qualification
 ![alt text](charts/loan_status_education.png)
 
+
 3. Loan Status According to Loan Intent
 ![alt text](charts/loan_status_intent.png)
 
-4. Age Range
-![alt text](charts/age_range.png)
 
-5. Income Range
+4. Income Range
 ![alt text](charts/income_range.png)
 
-6. Loan Range
+
+5. Loan Range
 ![alt text](charts/loan_range.png)
 
-7. Credit Range
+
+6. Credit Range
 ![alt text](charts/credit_range.png)
 
-8. Income vs Loan Amount
+
+7. Income vs Loan Amount
 ![alt text](charts/income_vs_loan.png)
 
-9. HeatMap
+
+8. HeatMap
 ![alt text](charts/heatmap.png)
+
 
 ## Data Processing
     1. Label Encoding is performed for categorical data
@@ -52,4 +58,5 @@ This project predicts loan approval status using machine learning classification
     4. Hyperparameter Tunning using GridSearchCV
         - Accuracy: 93.35% and Cross Validation: 92.67%
     5. Model Evaluation is performed classification Report and Confusion Matrix
+    
 ![alt text](charts/confusion_matrix.png)
--- main.py
+++ main.py
@@ -11,11 +11,9 @@ with open('random_forest.pkl', 'rb') as model_file:
 
 st.title('Loan Approval Prediction')
 
-person_age = st.number_input('Age: ', max_value=100, value=None)
 person_gender = st.selectbox('Gender: ', encoders['person_gender'].classes_)
 person_education = st.selectbox('Education Qualification: ', encoders['person_education'].classes_)
 person_income = st.number_input('Income: ', value=100)
-person_emp_exp = st.number_input('Employment Experience: ', value=None)
 person_home_ownership = st.selectbox('Home Ownership: ', encoders['person_home_ownership'].classes_)
 loan_amnt = st.number_input('Loan Amount: ', value=100)
 loan_intent = st.selectbox('Loan Intent: ', encoders['loan_intent'].classes_)
@@ -25,16 +23,13 @@ credit_score = st.number_input('Credit Score: ', value=None)
 previous_loan_defaults_on_file = st.selectbox('Previous Loan Defaults: ', encoders['previous_loan_defaults_on_file'].classes_)
 
 loan_percent_income = loan_amnt / person_income 
-income_to_loan = person_income / loan_amnt 
 net_disposable_income = person_income - (loan_amnt * loan_int_rate / 100)
 
 
 encoded_data = {
-    'person_age': person_age,
     'person_gender': encoders['person_gender'].transform([person_gender])[0],
     'person_education': encoders['person_education'].transform([person_education])[0],
     'person_income': person_income,
-    'person_emp_exp': person_emp_exp, 
     'person_home_ownership': encoders['person_home_ownership'].transform([person_home_ownership])[0],
     'loan_amnt': loan_amnt,
     'loan_intent': encoders['loan_intent'].transform([loan_intent])[0], 
@@ -43,12 +38,11 @@ encoded_data = {
     'cb_person_cred_hist_length': cb_person_cred_hist_length,
     'credit_score': credit_score, 
     'previous_loan_defaults_on_file': encoders['previous_loan_defaults_on_file'].transform([previous_loan_defaults_on_file])[0],     
-    'income_to_loan': income_to_loan, 
     'net_disposable_income': net_disposable_income 
 }
 
 predict_df = pd.DataFrame([encoded_data])
-scaler = ['person_age', 'person_income', 'loan_amnt', 'loan_int_rate','loan_percent_income', 'cb_person_cred_hist_length', 'credit_score', 'income_to_loan', 'net_disposable_income']
+scaler = ['person_income', 'loan_amnt', 'loan_int_rate','loan_percent_income', 'cb_person_cred_hist_length', 'credit_score', 'net_disposable_income']
 # scaler = StandardScaler()
 predict_df[scaler] = StandardScaler().fit_transform(predict_df[scaler])
 
